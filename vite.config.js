52cc286fcc7e644e193bdbf2dd5bc4afbdd892a9
Maguofu123 <815070571@qq.com>
2023-06-27 14:43:37 +0800

    制定主题色

--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,32 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    vue(),
      // ...
      AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({importStyle: "sass"})],
     }),
-    vue(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
