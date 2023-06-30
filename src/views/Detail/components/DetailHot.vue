dce890a8a8c0194f0022e19d630f4a89899f8414
Maguofu123 <815070571@qq.com>
2023-06-30 11:55:01 +0800

    使用props传值和computed计算属性实现适配不同标题title和列表不同内容

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -1,14 +1,32 @@
 <script setup>
 // 获取数据 渲染模板
 import { getHotGoodsAPI } from '@/apis/detail.js'
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { useRoute } from 'vue-router'
+
+// 设计props参数 适配不同的title和数据
+const props = defineProps({
+    hotType: {
+        type: Number
+    }
+})
+
+// 适配title 1 - 24小时热榜 2 - 周热榜 
+const TYPEMAP = {
+    1: '24小时热榜',
+    2: '周热榜'
+}
+
+const title = computed(() => TYPEMAP[props.hotType])
+
+
+// 热榜区接收数据 渲染
 const hotList = ref([])
 const route = useRoute()
 const getHotList = async () => {
     const res = await getHotGoodsAPI({
         id: route.params.id,
-        type: 1
+        type: props.hotType
     })
     hotList.value = res.result
 }
@@ -21,7 +39,7 @@ onMounted(() => {
 
 <template>
     <div class="goods-hot">
-        <h3>周日榜单</h3>
+        <h3>{{ title }}</h3>
         <!-- 商品区块 -->
         <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
             <img :src="item.picture" alt="" />
