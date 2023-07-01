5e98823058df34303a3e018711ab9fedbc70e645
Maguofu123 <815070571@qq.com>
2023-07-01 11:49:00 +0800

    表单校验实现--表单对象，规则对象

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,24 @@
 <script setup>
+import { ref } from 'vue';
 
+// 表单校验 账户名 + 密码
+
+// 1. 准备表单对象
+const form = ref({
+  account: '',
+  password: '',
+})
+
+// 2. 准备规则对象
+const rules = {
+  account: [
+    { required: true, message: '用户名不能为空', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur' },
+    { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' }
+  ]
+}
 </script>
 
 
@@ -24,16 +43,18 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
-              status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+            <!-- 绑定表单对象和规则对象 -->
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+              <!-- 绑定使用的规则字段 -->
+              <el-form-item prop="account" label="账户">
+                <!-- 双向绑定表单数据 -->
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+                <el-checkbox size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
