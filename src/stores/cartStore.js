0aeaf118288994a654aeaf2b2e150e72dc2fa8f8
Maguofu123 <815070571@qq.com>
2023-07-09 21:32:39 +0800

    购物车列表统计数据并添加最小数量为1

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -42,8 +42,10 @@ export const useCartStore = defineStore('cart', () => {
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count ,0))
     // 2. 总价
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price ,0))
-    
-    
+    // 3. 已选择数量
+    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count ,0))
+    // 4. 已选择商品价钱合计
+    const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count * c.price ,0))
     // 是否全选
     const isAll = computed(() => cartList.value.every((item) => item.selected))
     
@@ -61,7 +63,9 @@ export const useCartStore = defineStore('cart', () => {
         allPrice,
         singleCheck,
         isAll,
-        allCheck
+        allCheck,
+        selectedCount,
+        selectedPrice
     }
 
 },{
